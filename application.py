b2b341fca793d7c56ab6d81194d936abff1df471
taewan <ntw2100@gmail.com>
2022-04-27 16:48:31 +0900

    에러 테스트

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
 @application.route('/')
 def main():
-    return render_template("index.html")
+    return "hello"
 
 @application.route('/fileupload', methods=['POST'])
 def file_upload():
